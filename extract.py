19c5fcb46c0ad36ec59e25f9b1b0b4314b62cb8c
Aditya Sharma <aditya29032005@gmail.com>
2026-04-04 22:13:32 +0530

    Updated extract.py

--- extract.py
+++ extract.py
@@ -1,5 +1,6 @@
 import os
 import fitz
+import json
 
 count = 0
 base_dir = "DataSet"
@@ -23,8 +24,8 @@ def is_criminal_case(txt):
 
     return False
 
-for y in os.listdir(base_dir):
-    y_path = os.path.join(base_dir, y)
+for year in os.listdir(base_dir):
+    y_path = os.path.join(base_dir, year)
 
     if os.path.isdir(y_path):
         for file in os.listdir(y_path):
@@ -34,12 +35,24 @@ for y in os.listdir(base_dir):
                 text = extract_text(pdf_path)
 
                 if is_criminal_case(text):
-                    print(f"{y} criminal")
+                    print(f"{year} criminal")
+
+                    data = {
+                        "file": file,
+                        "year": year,
+                        "text": text
+                    }
+
+                    with open("criminal_cases.json", "a", encoding="utf-8") as f:
+                        json.dump(data, f)
+                        f.write("\n")
+
                     count += 1
                 else:
-                    print(f"{y} no")
+                    os.remove(pdf_path)
+                    print(f"deleted file {pdf_path} from {year}")
 
             except Exception as e:
                 print(f"Error reading file{file}: {e}")
 
-print(count)
+print(f"Number of Files of criminal cases: {count}")
